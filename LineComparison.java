e603cfbcc3da6212529c24e77945e21562f0fb2a
ayurn <65171090+ayurn@users.noreply.github.com>
2021-04-24 18:27:23 +0530

    Added : Equality of lines

--- LineComparison.java
+++ LineComparison.java
@@ -23,5 +23,18 @@ public class LineComparison {
 
   	    double len2 = Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
   	    System.out.println(" Length of line2 is : "+len2);
+
+  	    String strlen1 = String.valueOf(len1);
+  	    String strlen2 = String.valueOf(len2);
+
+  	    int comparison=strlen1.compareTo(strlen2);
+  	    System.out.println("Comparison of Line1 and Line2 = "+comparison);
+  	    if(comparison==0)
+  	    {
+  	    	System.out.println("Length of Line1 "+len1+" is equal to Length of Line2 "+len2 );
+  	    	
+  	    }
+
+
 	}
 }
