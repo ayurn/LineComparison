75ae995df6ab8ca86790eb13f8a90c2464bd0d85
ayurn <65171090+ayurn@users.noreply.github.com>
2021-04-27 19:42:41 +0530

    Refactored : Spacing

--- LineComparison.java
+++ LineComparison.java
@@ -7,41 +7,34 @@ public class LineComparison {
 		double x1, y1, x2, y2;
   	    Scanner sc = new Scanner(System.in);
   	    System.out.println(" Enter co-ordinates of first line : ");
-  	    x1=sc.nextDouble();
-  	    y1=sc.nextDouble();
-  	    x2=sc.nextDouble();
-  	    y2=sc.nextDouble();
+  	    x1 = sc.nextDouble();
+  	    y1 = sc.nextDouble();
+  	    x2 = sc.nextDouble();
+  	    y2 = sc.nextDouble();
 
-  	    double len1 = Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
-  	    System.out.println(" Length of line1 is : "+len1);
+  	    double length1 = Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
+  	    System.out.println(" Length of line1 is : "+length1);
 
   	    System.out.println(" Enter co-ordinates of second line : ");
-  	    x1=sc.nextDouble();
-  	    y1=sc.nextDouble();
-  	    x2=sc.nextDouble();
-  	    y2=sc.nextDouble();
+  	    x1 = sc.nextDouble();
+  	    y1 = sc.nextDouble();
+  	    x2 = sc.nextDouble();
+  	    y2 = sc.nextDouble();
 
-  	    double len2 = Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
-  	    System.out.println(" Length of line2 is : "+len2);
+  	    double length2 = Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
+  	    System.out.println(" Length of line2 is : "+length2);
 
   	    String strlen1 = String.valueOf(len1);
   	    String strlen2 = String.valueOf(len2);
 
-  	    int comparison=strlen1.compareTo(strlen2);
+  	    int comparison = strlen1.compareTo(strlen2);
   	    System.out.println("Comparison of Line1 and Line2 = "+comparison);
-  	    if(comparison==0)
-  	    {
-  	    	System.out.println("Length of Line1 = "+len1+" is equal to Length of Line2 = "+len2);
+  	    if (comparison==0) {
+  	    	System.out.println("Length of Line1 = "+length1+" is equal to Length of Line2 = "+length2);
+	    } else if(comparison<0) {
+  	     	System.out.println("Length of Line1 = "+length1+" is less than to Length of Line2 = "+length2);	
+  	    } else {
+  	      	System.out.println("Length of Line1 = "+length1+" is greater than to Length of Line2 = "+length2);
   	    }
-  	    else if(comparison<0)
-  	    {
-  	    	System.out.println("Length of Line1 = "+len1+" is less than to Length of Line2 = "+len2);	
-  	    }
-  	    else
-  	    {
-  	    	System.out.println("Length of Line1 = "+len1+" is greater than to Length of Line2 = "+len2);
-  	    }
-
-
 	}
 }
